964ef68289b6dce8b9be71e4fe7ddea34b7e44c9
RobertoFioravanti <133392154+RobertoFioravanti@users.noreply.github.com>
2023-08-28 18:21:38 +0200

    Update send-email.py

--- send-email.py
+++ send-email.py
@@ -31,11 +31,8 @@ parser.add_argument(
     required=True,
     help="Email address to send the notification to",
 )
-parser.add_argument(
-    "--subject", type=str, required=True, help="Subject of the email")
-parser.add_argument(
-    "--markdown-body", type=str, required=True, help="Body of the email"
-)
+parser.add_argument("--subject", type=str, required=True, help="Subject of the email")
+parser.add_argument(    "--markdown-body", type=str, required=True, help="Body of the email")
 parser.add_argument(
     "--from",
     dest="from_email",
@@ -43,20 +40,17 @@ parser.add_argument(
     required=True,
     help="Email address to send the notification from",
 )
-parser.add_argument(
-    "--api-key", type=str, required=True, help="SendGrid API key"
-)
+parser.add_argument("--api-key", type=str, required=True, help="SendGrid API key")
 
 parser.add_argument(
-    "--attachments", type=str, dest="attachments",default='', nargs="+", required=False, help="attachments"
+    "--attachments", type=str, dest="attachments", default='', nargs="+", required=False, help="attachments"
 )
 
 if __name__ == "__main__":
-
     args = parser.parse_args()
     print("to_email:")
     print(args.to_email)
-    to=[e for item in args.to_email for e in item.split(" ")]
+    to = [e for item in args.to_email for e in item.split(" ")]
     
     print("to:")
     print(to)
@@ -67,23 +61,16 @@ if __name__ == "__main__":
         subject=args.subject,
         html_content=markdown.markdown(args.markdown_body),
     )
-
     
     A = [path for item in args.attachments for path in item.split(" ")]
     print("attachments entries:")
     print(A)
     
     if len(A):
-        
-        
-        
         for fname in A:
-
-            for f in glob.iglob(fname): # generator, search immediate subdirectories 
+            for f in glob.iglob(fname):  # generator, search immediate subdirectories 
                 print("fname:")
                 print(f)
-            
-                #basename = os.path.basename(fname)
                 basename = os.path.basename(f)
                
                 print("basename:")
@@ -96,13 +83,10 @@ if __name__ == "__main__":
                     file_type=mimetypes.guess_type(basename)[0],
                     file_name=basename,
                     disposition="attachment",
-                    content_id=f"<{basename}>"
+                    content_id=f"<{basename}>",
                 )
-        
                 message.add_attachment(attachment)
-      
-
-    
+                
     try:
         sg = SendGridAPIClient(args.api_key)
         response = sg.send(message)

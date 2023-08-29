4c35de521d1317a423ca540034839556948c6e29
RobertoFioravanti <133392154+RobertoFioravanti@users.noreply.github.com>
2023-08-29 13:57:25 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,13 +1,16 @@
+import sys, os.path  
 import unittest
+src_path = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/src/')
+sys.path.append(src_path)
 from sendmail import make_list
 
 class TestListElements(unittest.TestCase):
     def setUp(self):
-        self.expected = ['welcome', 'to', 'the', 'jungle']
+        self.expected = ["welcome", "to", "the", "jungle"]
         self.test = "welcome,\nto , the\n, \n  jungle\n"
 
     def test_makelist_ShouldReturnTheExpectedList(self):
-        """ """
+        """Test 01"""
         self.assertListEqual(make_list(self.test), self.expected)
 
 

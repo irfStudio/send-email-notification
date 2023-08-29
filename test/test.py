5d5d316936760fc9bab488e626fb1cfb08f0ed06
RobertoFioravanti <133392154+RobertoFioravanti@users.noreply.github.com>
2023-08-29 11:55:16 +0200

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,15 @@
+import unittest
+import send-email
+
+class TestListElements(unittest.TestCase):
+    def setUp(self):
+        self.expected = ['welcome', 'to', 'the', 'jungle']
+        self.test = "welcome,\nto , the\n, \n  jungle\n"
+
+    def test_makelist_ShouldReturnTheExpectedList(self):
+        """ """
+        self.assertListEqual(make_list(self.test), self.expected)
+
+
+if __name__ == "__main__":
+    unittest.main()

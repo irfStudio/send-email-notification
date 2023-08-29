89dbfdc3287b1e53c82ef627de1e71deaad0e77e
RobertoFioravanti <133392154+RobertoFioravanti@users.noreply.github.com>
2023-08-29 14:18:29 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,11 +10,17 @@ from sendmail import make_list
 class TestListElements(unittest.TestCase):
     def setUp(self):
         self.expected = ["welcome", "to", "the", "jungle"]
+        
         self.test = "welcome,\nto , the\n, \n  jungle\n"
+        self.test2 = "welcome\nto;the;;, \n  jungle\n"
 
-    def test_makelist_ShouldReturnTheExpectedList(self):
+    def test_makelist_ShouldReturnTheExpectedList_1(self):
         """Test 01"""
         self.assertListEqual(make_list(self.test), self.expected)
+        
+    def test_makelist_ShouldReturnTheExpectedList_2(self):
+        """Test 02"""
+        self.assertListEqual(make_list(self.test2), self.expected)
 
 
 if __name__ == "__main__":
